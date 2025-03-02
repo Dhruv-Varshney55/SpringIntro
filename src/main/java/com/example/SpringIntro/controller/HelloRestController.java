89fb0a518f935c26438be6541246da9f9ae5b754
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-03-02 15:38:11 +0530

    UC3

--- src/main/java/com/example/SpringIntro/controller/HelloRestController.java
+++ src/main/java/com/example/SpringIntro/controller/HelloRestController.java
@@ -21,4 +21,13 @@ public class HelloRestController {
     public String sayHello(@RequestParam String name) {
         return "Hello " + name + " from Bridgelabz!";
     }
+
+    // uc3
+    /* RequestParam -@RequestParam annotation to bind Servlet request parameters (that is, query parameters or form data) to a method argument in a controller.
+      return-Hello (name) from Bridgelabz
+      api- http://localhost:8080/params/dhruv */
+    @GetMapping("/params/{name}")
+    public String sayHelloPathVariable(@PathVariable String name) {
+        return "Hello " + name + " from Bridgelabz!";
+    }
 }
